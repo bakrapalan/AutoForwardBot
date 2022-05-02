6b88429f32bce0648146b679a876ef895c50ceb4
bakrapalan <104255719+bakrapalan@users.noreply.github.com>
2022-05-02 19:13:57 +0530

    Update hindi.py

--- plugins/hindi.py
+++ plugins/hindi.py
@@ -5,12 +5,11 @@ from info import ADMINS, TARGET_CHANNEL
 chat = TARGET_CHANNEL
 @client.on(events.NewMessage(chat=chat)
 async def file_del(x):
-  d=0
-    if x.media and hasattr(x.media, "document"):
-        name = x.file.name
-        if not re.search("(?i)hindi", name):
-            await x.delete()
-            d += 1
-  
-  await e.reply(f"Successfully deleted {d} non Hindi Files")
-  print(f"Deleted Total {d} files")
+           d=0
+           if x.media and hasattr(x.media, "document"):
+           name = x.file.name
+           if not re.search("(?i)hindi", name):
+           await x.delete()
+           d += 1
+           await e.reply(f"Successfully deleted {d} non Hindi Files")
+           print(f"Deleted Total {d} files")
